84d23064af1d615dbf8392916c688276b60cf130
Mthokozisi <tloumtho@gmail.com>
2023-02-17 16:47:46 -0800

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s = 97, e = 122;
+
+	while (s <= e)
+	{
+		putchar(s);
+		s++;
+	}
+
+	s = 65;
+	e = 90;
+
+	while (s <= e)
+	{
+		putchar(s);
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
