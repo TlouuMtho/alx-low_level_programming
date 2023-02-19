d804cb2583e343baca958cbe7e7b8066dbbb7155
Mthokozisi <tloumtho@gmail.com>
2023-02-19 06:32:17 -0800

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Prints the alphabet inreverse order
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = "z"; letter >= "a"; letter--)
+	putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
