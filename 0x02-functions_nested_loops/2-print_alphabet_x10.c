bf6e545979d84cc55eb3536c7b6a1c75e873dbfc
Mthokozisi <tloumtho@gmail.com>
2023-02-21 06:50:25 -0800

    2. x10 The alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,9 @@ void print_alphabet_x10(void)
 
 	while (letter <= 9)
 	{
-	for (letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 
-	_putchar(letter);
-	_putchar('\n');
+			_putchar(letter);
+			_putchar('\n');
 	}
-	return (0);
 }
