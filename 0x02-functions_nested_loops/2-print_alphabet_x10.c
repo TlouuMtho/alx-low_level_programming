41bcdef6a3fc482119c16d84384fc546e4463c47
Mthokozisi <tloumtho@gmail.com>
2023-02-21 06:45:14 -0800

    2. 10x Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Prints the alphabets ten times in a row
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+	int count = 0;
+	char letter;
+
+	while (letter <= 9)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	_putchar(letter);
+	_putchar('\n');
+	}
+	return (0);
+}
