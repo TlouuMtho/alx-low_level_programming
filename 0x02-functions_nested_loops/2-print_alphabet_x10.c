fac3139b68e8f04ecb1410ca5dfc82cf930967fe
Mthokozisi <tloumtho@gmail.com>
2023-02-21 07:36:18 -0800

    2. x10 The alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * main - Prints the alphabets ten times in a row
- *
- * Return: Always 0.
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
