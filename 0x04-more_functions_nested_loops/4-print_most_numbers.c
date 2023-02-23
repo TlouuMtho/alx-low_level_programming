fae5e04f14391beb86c7cc86ce85f7d683e62dac
Mthokozisi <tloumtho@gmail.com>
2023-02-23 13:45:50 -0800

    4. I'm a believer in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,17 +2,19 @@
 
 /**
  * print_most_numbers - prints the numbers from 0 to 9 except 2 and 4
+ * @c: character variable to be printed
  *
  * Return: void
  */
+
 void print_most_numbers(void)
 {
 	char c;
 
 	for (c = '0'; 0 <= '9'; c++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
-			_putchar('\n');
+	_putchar('\n');
 }
