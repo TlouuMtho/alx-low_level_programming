23da0714fbd1d8fc31f1809aeb07a3faeacd71ad
Mthokozisi <tloumtho@gmail.com>
2023-02-27 09:03:43 -0800

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value to of a variable to 98
+ * @n: int to update
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A readme explainign the functions of the functions created in the directory
