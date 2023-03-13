7abef6f6c7b52888013c486013d8021eccc9f5b9
Mthokozisi <tloumtho@gmail.com>
2023-03-13 08:48:27 -0700

    Static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,10 @@
 #include "main.h"
-
+/**
+ * _puts - prints string
+ * @s: string
+ * Return: 0
+ */
 void _puts(char *s)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
 }
